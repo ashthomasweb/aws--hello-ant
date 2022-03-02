2050f4412671e6bee5a5d9f506993f0cdffddf45
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-03-01 23:43:12 -0500

    Initial Ant layout
    Created a basic layout from AntD components. Very nice. removed ReactDOM
    and passed to App.js as component. Created basic link updating state and
    setting a value in the content area.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import SiderDemo from './layout/primary.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SiderDemo />
     </div>
   );
 }

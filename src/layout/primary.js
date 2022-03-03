ab988ab6395b98c562d4cf8c55809e009219df7b
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-03-03 00:02:54 -0500

    Corrects basic colors and layout
    Also creates basic text logo, and display handling during menu collapse
    via condtitional rendering.

--- src/layout/primary.js
+++ src/layout/primary.js
@@ -2,6 +2,8 @@
 import { React, Component } from 'react'
 
 import logo from '../assets/mill_logo.png'
+import textLogo from '../assets/text-logo.png'
+
 
 import { Layout, Menu, Breadcrumb } from 'antd';
 import {
@@ -9,13 +11,13 @@ import {
   PieChartOutlined,
   FileOutlined,
   TeamOutlined,
-  UserOutlined,
+  // UserOutlined,
 } from '@ant-design/icons';
 
 import './primary.css'
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+// const { SubMenu } = Menu;
 
 class SiderDemo extends Component {
   state = {
@@ -24,7 +26,6 @@ class SiderDemo extends Component {
   };
 
   onCollapse = collapsed => {
-    console.log(collapsed);
     this.setState({ collapsed });
   };
 
@@ -33,37 +34,34 @@ class SiderDemo extends Component {
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-          <div className="logo" ><img src={logo} alt='Windmill logo'/></div>
+          <div className="logo" >
+            <img src={logo} alt='Windmill logo'/>
+            {!this.state.collapsed && (<img className='text-logo' src={textLogo} alt='Skyline text logo'/>)}
+          </div>
           <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />} style={{ marginTop: '30px'}}>
-              Option 1
+            <Menu.Item key="1" icon={<TeamOutlined />} style={{ marginTop: '30px'}}>
+              Fleet
             </Menu.Item>
             <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
+              Inspections
             </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item onClick={() => this.setState({user: 'Tom'})} key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
+            <Menu.Item key="10" icon={<PieChartOutlined />}>
+              Work Orders
+            </Menu.Item>
             <Menu.Item key="9" icon={<FileOutlined />}>
-              Files
+              Analytics
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0, backgroundColor: 'lightblue' }} />
+          <Header className="header" />
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>{this.state.user}</Breadcrumb.Item>
             </Breadcrumb>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              This is a mock application built in the style of an existing application using promotional material as a model.
+              This is a mock application being built in the style of an existing application, using promotional material as a model.
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>UI built with Ant Design</Footer>
